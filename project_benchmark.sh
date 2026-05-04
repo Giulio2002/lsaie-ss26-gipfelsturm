31596dfe86bc2b6afe4a24e8306d2fc84b9b7266
Giulio rebuffo <giulio.rebuffo@gmail.com>
2026-05-04 18:47:41 +0200

    Add benchmark script for throughput testing

---
+++ project_benchmark.sh
@@ -0,0 +1,6 @@
+./launch.sh throughput 125m 50 1
+./launch.sh throughput 125m 50 2
+./launch.sh throughput 125m 50 4
+./launch.sh throughput 125m 100 1
+./launch.sh throughput 125m 100 2
+./launch.sh throughput 125m 100 4
